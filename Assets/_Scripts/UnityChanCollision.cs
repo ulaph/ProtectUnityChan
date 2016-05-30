449ee37b5bbfd0c851d3737ac1f2b3d77f646764
sho Hashimoto <cisy13112@g.nihon-u.ac.jp>
2016-05-30 22:12:14 +0900

    Add UnityChanCollision.

---
+++ Assets/_Scripts/UnityChanCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanCollision : MonoBehaviour
+{
+
+    [SerializeField] Animator ownAnim;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.transform.tag)
+        {
+            case "Enemy":
+                ownAnim.SetBool("GameOver", true);
+                break;
+        }
+    }
+}
