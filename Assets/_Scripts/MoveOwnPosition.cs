8f9dbb5129455e5f90e4095e6a70d0a7576ee21a
sho Hashimoto <cisy13112@g.nihon-u.ac.jp>
2016-06-03 14:09:44 +0900

    Add RaySystem.

--- Assets/_Scripts/MoveOwnPosition.cs
+++ Assets/_Scripts/MoveOwnPosition.cs
@@ -1,15 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UniRx;
+using UniRx.Triggers;
 
-public class MoveOwnPosition : MonoBehaviour {
+public class MoveOwnPosition : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    SteamVR_TrackedObject trackedObj;
+    [SerializeField] GameObject ownPlayer;
+    [SerializeField] GameObject targetPointer;
+    Vector3 movePos;
+
+    void Start()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        targetPointer.SetActive(false);
+        this.UpdateAsObservable()
+            .Where(_ => isTriggerDown())
+            .Subscribe(_ => dropRay());
+
+        this.UpdateAsObservable()
+            .Where(_ => isTriggerUp())
+            .Subscribe(_ => moveTOPoint());
+    }
+
+    bool isTriggerDown()
+    {
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool isTriggerUp()
+    {
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void dropRay()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Camera.main.farClipPlane))
+        {
+            movePos = hit.point;
+            targetPointer.SetActive(true);
+            targetPointer.transform.position = movePos;
+        }
+    }
+
+    void moveTOPoint()
+    {
+        ownPlayer.transform.position = movePos;
+    }
 }
